9f5c6d2222b9cb22a88bd2a7f4101ba6f4739202
Luke7622 <luke@sqlservermigrations.com>
2018-11-13 14:41:33 -0600

    Create create-dba-database.sh

---
+++ create-dba-database.sh
@@ -0,0 +1,5 @@
+#wait for the SQL Server to come up
+sleep 90s
+
+#run the setup script to create the DB and the schema in the DB
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P YourStrongP@ssword -d master -i create-dba-database.sql
