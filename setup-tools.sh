e6981bf3ceaed2bba4872ab3ef496ee1ac3af889
Luke7622 <luke@sqlservermigrations.com>
2018-11-13 16:23:16 -0600

    Update setup-tools.sh

--- setup-tools.sh
+++ setup-tools.sh
@@ -2,4 +2,11 @@
 sleep 30s
 
 #run the setup script to create the DB and the schema in the DB
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P YourStrongP@ssword -d master -i create-dba-database.sql
+sqlfiles=( create-dba-database.sql tools-permission-snapshot.sql tools-ola-maintenancesolution.sql tools-sp-whoisactive.sql )
+
+for file in "${sqlfiles[@]}"
+do 
+ /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P YourStrongP@ssword -d master -i "${file}"
+ sleep 1
+done
+
