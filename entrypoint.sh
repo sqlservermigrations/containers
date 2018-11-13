7f883a3aaca7df6dd97170e4e43e0d04fb6b904b
Luke7622 <luke@sqlservermigrations.com>
2018-11-13 16:13:11 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #start SQL Server, start the script to create the DB and import the data, start the app
-/usr/src/scripts/create-dba-database.sh & /opt/mssql/bin/sqlservr
+/usr/src/scripts/setup-tools.sh & /opt/mssql/bin/sqlservr
